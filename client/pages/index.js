5837b2ac9d40e78d0bd7fbdea50a423756f3c1f1
SP_Penguin <abrcdf1023@gmail.com>
2020-02-04 23:23:38 +0800

    [client] basic demo done

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,31 @@
-import Alert from '@material-ui/lab/Alert';
-import Button from '@material-ui/core/Button'
+import Layout from '../components/MyLayout'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-import Layout from "../components/Layout";
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/show/[id]" as={`/show/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
 
-export default function Index() {
-  return (
-    <Layout>
-      <div>
-        <Alert severity="info">This is a warning alert — check it out!</Alert>
-        &nbsp; <Button color="primary">Hello from nextjs</Button>
-      </div>
-    </Layout>
-  );
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
 }
+
+export default Index
